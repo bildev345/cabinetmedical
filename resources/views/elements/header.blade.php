1e20c9515c611201b94a1411f2f80664c6dfca2d
Hamza-Hmidani <hamidanh855@gmail.com>
2025-02-28 20:05:35 +0000

    modifier le header

--- resources/views/elements/header.blade.php
+++ resources/views/elements/header.blade.php
@@ -1,16 +1,15 @@
-
- <!-- HEADER -->
- <header>
+<!-- HEADER -->
+<header>
     <div class="container">
          <div class="row">
 
               <div class="col-md-4 col-sm-5">
-                   <p>Welcome to a Professional Health Care</p>
+                   <p>Bienvenue dans un centre de santé professionnel</p>
               </div>
 
               <div class="col-md-8 col-sm-7 text-align-right">
                    <span class="phone-icon"><i class="fa fa-phone"></i> 010-060-0160</span>
-                   <span class="date-icon"><i class="fa fa-calendar-plus-o"></i> 6:00 AM - 10:00 PM (Mon-Fri)</span>
+                   <span class="date-icon"><i class="fa fa-calendar-plus-o"></i> 6:00 AM - 10:00 PM (Lun-Ven)</span>
                    <span class="email-icon"><i class="fa fa-envelope-o"></i> <a href="#">info@company.com</a></span>
               </div>
 
