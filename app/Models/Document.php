de430adcb5c65f5ef6c1a9b7b303074a608e93c5
retoufik <wachani3@outlook.com>
2025-03-01 14:26:57 +0000

    missing imports

--- app/Models/Document.php
+++ app/Models/Document.php
@@ -4,10 +4,13 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\Models\TypeDocument;
+use App\Models\Patient;
 
 class Document extends Model
 {
     use HasFactory;
+    public $timestamps = false;
     protected $disk = 'local'; 
     protected $guarded  = ["id"];
 
--- app/Models/TypeDocument.php
+++ app/Models/TypeDocument.php
@@ -4,10 +4,12 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\Models\Document;
 
 class TypeDocument extends Model
 {
     use HasFactory;
+    public $timestamps = false;
     protected $guarded = ["id"];
 
     public function documents(){
